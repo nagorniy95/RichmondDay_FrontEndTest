23dc7c7591c6ea0515dd6f4d6c30e7dd36f2dc77
Artem Nahornyi <nagornyi95@gmail.com>
2019-10-02 21:25:29 -0400

    design done

--- root/js/main.js
+++ root/js/main.js
@@ -17,20 +17,22 @@ const displayData = (data) => {
     let carList = data.reduce((list, item, index) => {
         const h3Class = index % 2 === 0 ? 'odd' : 'even';
         const currentItem = `<div class="col-md-6">
+                             <div id="json_content_block">
                                 <a href="#" targer="_blank">
                                     <h3>${item.Name}</h3>
                                 </a>
-                                <p>${item.Price}</p>
+                                <p class="price">${item.Price}</p>
                                 <img width="100%" height="auto" src="img/99b0164a-089e-400b-9b89-42718e30db4a.jpg" alt="mini cupper">
-                                <p>${item.Retailer}</p>
-                                <p>${item.Kilometres}</p>
-                                <p>${item.Transmission}</p>
-                                <p>${item.Exterior}</p>
-                                <p>${item.Interior}</p>
-                                <p>${item.VIN}</p>
-                                <p>${item.DriveTrain}</p>
+                                <p><span class="bold">Retailer:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ${item.Retailer}</p>
+                                <p><span class="bold">Kilometres:&nbsp;&nbsp;&nbsp;&nbsp;</span> ${item.Kilometres}</p>
+                                <p><span class="bold">Transmission: </span> ${item.Transmission}</p>
+                                <p><span class="bold">Exterior:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ${item.Exterior}</p>
+                                <p class="interior"><span class="bold">Interior:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ${item.Interior}</p>
+                                <p><span class="bold">VIN:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ${item.VIN}</p>
+                                <p class="drive_train"><span class="bold">DriveTrain:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ${item.DriveTrain}</p>
                                 <button>View Details</button>
                                 <button>Book Test Drive</button>
+                            </div>
                             </div>`;    
         list += currentItem;
         return list;
